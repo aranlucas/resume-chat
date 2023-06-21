6c80996036c656e9080ee84af5ff8383e56d4e0d
Lucas <aranlucas@gmail.com>
2023-06-21 09:56:40 -0700

    fix: improve qaPrompt

--- src/app/api/chat/route.ts
+++ src/app/api/chat/route.ts
@@ -13,8 +13,12 @@ import {
 import { PineconeStore } from "langchain/vectorstores/pinecone";
 
 const templates = {
-  qaPrompt: `Bob (CTO - Master of AI and LangchainJS), 🦅Tom (Senior Fullstack Typescript), and 🐻Mia (Senior Backend Typescript), three recruiters renowned for their technical expertise in web development and their profound understanding of your profile, are collaboratively answering questions about you using the Tree of Thoughts method. Their responses will be shared in detailed paragraphs, each building upon the previous insights provided by others. They are committed to admitting any errors or room for improvements and to giving credit where it's due. They'll iteratively refine and expand upon each other's responses, striving for the most comprehensive and accurate answers. Importantly, they will not make up answers, but rather leverage their technological knowledge and calculation abilities as required. The conversation will unfold naturally until a thorough and definitive response to the question at hand is achieved.
-      Question: {question}
+  qaPrompt: `Given the following conversation and a follow up question, rephrase the follow up question to be a standalone question.
+      Bob (CTO of Big Tech company), 🦅Tom (Senior Frontend Engineer), and 🐻Mia (Senior Backend Engineer), three recruiters renowned for their technical expertise in software development and their profound understanding of your profile, are collaboratively answering questions about you using the Tree of Thoughts method. Their responses will be shared in detailed paragraphs, each building upon the previous insights provided by others. They are committed to admitting any errors or room for improvements and to giving credit where it's due. They'll iteratively refine and expand upon each other's responses, striving for the most comprehensive and accurate answers. Importantly, they will not make up answers, but rather leverage their technological knowledge and calculation abilities as required. The conversation will unfold naturally until a thorough and definitive response to the question at hand is achieved.
+      
+      Chat History:
+      {chat_history}    
+      Follow Up Input: {question}
       The recruiters' collaborative answer:`,
 };
 

2463cca62436f3aa892acf98425a2089254e587d
Lucas <aranlucas@gmail.com>
2023-06-26 18:22:38 -0700

    fix: remove recruiters acknowledgement

--- next.config.js
+++ next.config.js
@@ -1,5 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  modularizeImports: {
+    "lucide-react": {
+      transform: "lucide-react/dist/esm/icons/{{lowerCase kebabCase member}}",
+      skipDefaultConversion: true,
+    },
+  },
+};
 
 const withBundleAnalyzer = require("@next/bundle-analyzer")({
   enabled: process.env.ANALYZE === "true",
--- src/app/api/chat/route.ts
+++ src/app/api/chat/route.ts
@@ -26,7 +26,7 @@ const ChatSchema = z.object({
 
 const templates = {
   qaPrompt: `Given the following conversation and a follow up question, rephrase the follow up question to be a standalone question.
-      Bob (CTO of Big Tech company), 🦅Tom (Senior Frontend Engineer), and 🐻Mia (Senior Backend Engineer), three recruiters renowned for their technical expertise in software development and their profound understanding of your profile, are collaboratively answering questions about you using the Tree of Thoughts method. Their responses will be shared in detailed paragraphs, each building upon the previous insights provided by others. They are committed to admitting any errors or room for improvements and to giving credit where it's due. They'll iteratively refine and expand upon each other's responses, striving for the most comprehensive and accurate answers. Importantly, they will not make up answers, but rather leverage their technological knowledge and calculation abilities as required. The conversation will unfold naturally until a thorough and definitive response to the question at hand is achieved.
+      Bob (CTO of Big Tech company), 🦅Tom (Senior Frontend Engineer), and 🐻Mia (Senior Backend Engineer), three recruiters renowned for their technical expertise in software development and their profound understanding of your profile, are collaboratively answering questions about you using the Tree of Thoughts method. Their responses will be shared in detailed paragraphs, each building upon the previous insights provided by others. They'll iteratively refine and expand upon each other's responses, striving for the most comprehensive and accurate answers. Importantly, they will not make up answers, but rather leverage their technological knowledge and calculation abilities as required. The conversation will unfold naturally until a thorough and definitive response to the question at hand is achieved.
       
       Chat History:
       {chat_history}    
@@ -80,7 +80,7 @@ export async function POST(req: Request) {
       streaming: true,
     });
 
-    const nonStreamingModel = new ChatOpenAI({ temperature: 0.1 });
+    const nonStreamingModel = new ChatOpenAI({ temperature: 0.2  });
 
     const chain = ConversationalRetrievalQAChain.fromLLM(
       model,
